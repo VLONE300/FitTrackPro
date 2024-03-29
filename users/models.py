8100b14a85659b4b77ac99c393ffc779d6b4cb31
VLONE <ermolich158@gmail.com>
2024-03-29 19:10:46 +0300

    first init

--- users/models.py
+++ users/models.py
@@ -3,8 +3,6 @@ from django.contrib.auth.models import AbstractUser, BaseUserManager, Permission
 
 
 class CustomUserManager(BaseUserManager):
-    """Как имеено регается юзер"""
-
     def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError('Email field must be set')
@@ -20,17 +18,17 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
-        return self.create_user(email, password, **extra_fields)
+        return self.create_user(email,password, **extra_fields)
 
 
 class CustomUser(AbstractUser, PermissionsMixin):
+    username = models.CharField(max_length=150, unique=True, blank=True, null=True)
     email = models.EmailField(unique=True)
     phone_number = models.CharField(max_length=20, blank=True, null=True, default=None)
     first_name = models.CharField(max_length=100, blank=True, null=True, default=None)
     last_name = models.CharField(max_length=100, blank=True, null=True, default=None)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_trainer = models.BooleanField(default=False)
     is_notification_required = models.BooleanField(default=True)
 
     USERNAME_FIELD = 'email'
@@ -40,3 +38,7 @@ class CustomUser(AbstractUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class Trainer(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
