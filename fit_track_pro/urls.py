4fc8bba59cb4ee5a505c2eb21373d3f13e43ff93
VLONE <ermolich158@gmail.com>
2024-04-13 11:15:09 +0300

    s

--- fit_track_pro/urls.py
+++ fit_track_pro/urls.py
@@ -20,9 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-
-
-
-
-    path('training-programs/', include('workouts.urls')),
 ]
