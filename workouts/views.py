d52c979deef83c6134c6c4d2e05103889c33787a
vlone <ermolich158@gmail.com>
2024-04-28 14:14:22 +0300

    resolved conflict

--- workouts/views.py
+++ workouts/views.py
@@ -30,6 +30,10 @@ class TrainingProgramViewSet(viewsets.ModelViewSet):
     queryset = TrainingProgram.objects.filter(training_type='General')
     serializer_class = TrainingProgramSerializer
 
+    def perform_create(self, serializer):
+        user = self.request.user
+        serializer.save(user=user)
+        send_create_program_mail(user.email)
 
 
 class MyProgramViewSet(viewsets.ModelViewSet):
